a499fd15725e27f74847ddaac3b4294ca3c5ef7f
benjo247 <benjaminlinge@googlemail.com>
2026-04-22 14:28:13 +0200

    Delete app directory

--- app/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div className="card">
-      <h2>Einstellungen</h2>
-      <div style={{ height: 12 }} />
-      <p className="muted">Für das MVP bleiben die Einstellungen zunächst statisch.</p>
-    </div>
-  );
-}
